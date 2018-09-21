65a713c686a4e030c098324aef16e4688cc51765
Evan Cui <dnetdvp@163.com>
2018-09-21 10:15:20 +0800

    Added the vmss extension ps file

---
+++ publish/AddExt.ps1
@@ -0,0 +1,9 @@
+﻿param (
+  [Parameter(Mandatory=$true)]
+  [Microsoft.Azure.Commands.Compute.Automation.Models.PSVirtualMachineScaleSet] $vmss
+)
+
+Add-AzureRmVmssExtension -VirtualMachineScaleSet $vmss -Name nodeagent1 -Publisher Microsoft.HpcPack -Type HpcAcmAgent -TypeHandlerVersion 1.0
+Update-AzureRmVmss -VirtualMachineScaleSet $vmss -ResourceGroupName $vmss.ResourceGroupName -VMScaleSetName $vmss.Name
+Update-AzureRmVmssInstance -ResourceGroupName $vmss.ResourceGroupName -VMScaleSetName $vmss.Name -InstanceId "*"
+
---
+++ publish/RemoveExt.ps1
@@ -0,0 +1,9 @@
+﻿param (
+  [Parameter(Mandatory=$true)]
+  [Microsoft.Azure.Commands.Compute.Automation.Models.PSVirtualMachineScaleSet] $vmss
+)
+
+Remove-AzureRmVmssExtension -VirtualMachineScaleSet $vmss -Name nodeagent1
+Update-AzureRmVmss -VirtualMachineScaleSet $vmss -ResourceGroupName $vmss.ResourceGroupName -VMScaleSetName $vmss.Name
+Update-AzureRmVmssInstance -ResourceGroupName $vmss.ResourceGroupName -VMScaleSetName $vmss.Name -InstanceId "*"
+

bb0601473ff1628be7ff010905d33fc15c9eb929
Evan Cui <dnetdvp@163.com>
2018-09-25 18:50:12 +0800

    add a fix ext file

---
+++ publish/FixExt.ps1
@@ -0,0 +1,19 @@
+﻿param (
+  [Parameter(Mandatory=$true)]
+  [string] $rg
+)
+
+$vms = Get-AzureRmVm -ResourceGroupName $rg
+foreach ($vm in $vms)
+{
+  Write-Host "Fixing $vm.Name"
+  try {
+    Remove-AzureRmVMExtension -ResourceGroupName $rg -VMName $vm.Name -Name "HpcAcmAgent" -Force
+    
+  }
+  catch {
+    Write-Host "failed to remove $vm.Name"
+  }
+  Set-AzureRmVMExtension -Publisher "Microsoft.HpcPack" -ExtensionType "HpcAcmAgent" -ResourceGroupName $rg -TypeHandlerVersion 1.0 -VMName $vm.Name -Location $vm.Location -Name "HpcAcmAgent"
+}
+

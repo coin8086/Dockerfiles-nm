d9bd2845cb1a4675aeba5b90bf6a7b85a048e849
Evan Cui <dnetdvp@163.com>
2019-01-22 23:25:52 +0800

    Added the vcredist installation

--- VMExtensionWin/handler.ps1
+++ VMExtensionWin/handler.ps1
@@ -31,6 +31,8 @@ switch ($action)
         $config.NodeRegisterWorkerOptions | Add-Member -Name "Enabled" -Value "True" -MemberType NoteProperty
         $config | ConvertTo-Json | Set-Content $configPath
 
+        Start-Process -FilePath "vcredist_x64.exe" -ArgumentList "/install /quiet /norestart /log vcredist.log" -Wait
+
         Log "Installing msi"
         Start-Process -FilePath "msiexec.exe" -ArgumentList "/i `"$PSScriptRoot\HpcNodeAgent_x64.msi`" ADDLOCAL=NodeAgent CCPDIR=`"$env:ProgramFiles\Microsoft HPC Pack ACM\`" DATADIR=`"$env:ProgramFiles\Microsoft HPC Pack ACM\Data\`"" -Wait
     }
